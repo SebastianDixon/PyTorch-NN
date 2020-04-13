dfafd0b46b1bf6b42338d79d8926345852d97357
Sebastian Dixon <smedixon@gmail.com>
2020-04-13 13:38:46 +0100

    main script, stepping into and over the forward function, relu algorithm

--- scripts/main.py
+++ scripts/main.py
@@ -1,6 +1,65 @@
-import torch 
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
 
-t = torch.tensor([1,2,3])
+torch.set_printoptions(linewidth=120)
+torch.set_grad_enabled(False)
 
-gpu = t.cuda()
-print(gpu)
+train_set = torchvision.datasets.FashionMNIST(
+    root='./data/FashionMNIST'
+    ,train=True
+    ,download=True
+    ,transform=transforms.Compose([
+        transforms.ToTensor()
+    ])
+)
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=5)
+
+
+class Network(nn.Module):
+    def __init__(self):
+        super(Network, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5)
+        self.conv2 = nn.Conv2d(in_channels=6, out_channels=12, kernel_size=5)
+
+        self.fc1 = nn.Linear(in_features=12*4*4, out_features=120)
+        self.fc2 = nn.Linear(in_features=120, out_features=60)
+        self.out = nn.Linear(in_features=60, out_features=10)
+
+    def forward(self, t):
+        t = F.relu(self.conv1(t))
+        t = F.max_pool2d(t, kernel_size=2, stride=2)
+
+        t = F.relu(self.conv2(t))
+        t = F.max_pool2d(t, kernel_size=2, stride=2)
+
+        t = t.reshape(-1, 12*4*4)
+        t = F.relu(self.fc1(t))
+        t = F.relu(self.fc2(t))
+        t = self.out(t)
+
+        return t
+
+
+def batch():
+    network = Network()
+    batch = next(iter(train_loader))
+    images, labels = batch
+    pred = network(images)
+
+    error = pred.argmax(dim=1).eq(labels).sum().item()
+
+    print(F.softmax(pred, dim=1), '\n')
+    print(pred.argmax(dim=1))
+    print(labels)
+    print(error, '\n')
+
+    return print(pred)
+
+network = Network()
+batch_item = next(iter(train_set))
+image, label = batch_item
+pred = network(image.unsqueeze(0))
+print(pred, '\n')
